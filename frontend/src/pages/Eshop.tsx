d0da93f7947c983ee740569b609a221c46e44909
antoinefoux-ui <antoine.foux@gmail.com>
2026-02-11 20:34:28 +0100

    Create Eshop.tsx

---
+++ frontend/src/pages/Eshop.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ProductCatalog from '../components/eshop/productcatalog';
+
+function Eshop() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-6">E-Shop</h1>
+      <ProductCatalog />
+    </div>
+  );
+}
+
+export default Eshop;
