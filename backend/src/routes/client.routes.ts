9556fb41e2f713f55ac6224290d2fda7db889124
antoinefoux-ui <antoine.foux@gmail.com>
2026-02-12 13:24:24 +0100

    Refactor routes to use arrow functions for handlers

--- backend/src/routes/client.routes.ts
+++ backend/src/routes/client.routes.ts
@@ -5,17 +5,17 @@ import ClientController from '../controllers/client.controller';
 const router = Router();
 
 // Public route for dashboard stats
-router.get('/', ClientController.getAll);
+router.get('/', (req, res) => ClientController.getAll(req, res));
 
 // Protected routes  
-router.post('/', authenticate, ClientController.create);
-router.get('/:id/communications', authenticate, ClientController.getCommunications);
-router.post('/:id/communications', authenticate, ClientController.sendCommunication);
-router.get('/:id/statistics', authenticate, ClientController.getStatistics);
-router.get('/:id/export', authenticate, ClientController.exportData);
-router.get('/:id', authenticate, ClientController.getById);
-router.put('/:id', authenticate, ClientController.update);
-router.delete('/:id', authenticate, ClientController.delete);
-router.post('/merge', authenticate, ClientController.merge);
+router.post('/', authenticate, (req, res) => ClientController.create(req, res));
+router.get('/:id/communications', authenticate, (req, res) => ClientController.getCommunications(req, res));
+router.post('/:id/communications', authenticate, (req, res) => ClientController.sendCommunication(req, res));
+router.get('/:id/statistics', authenticate, (req, res) => ClientController.getStatistics(req, res));
+router.get('/:id/export', authenticate, (req, res) => ClientController.exportData(req, res));
+router.get('/:id', authenticate, (req, res) => ClientController.getById(req, res));
+router.put('/:id', authenticate, (req, res) => ClientController.update(req, res));
+router.delete('/:id', authenticate, (req, res) => ClientController.delete(req, res));
+router.post('/merge', authenticate, (req, res) => ClientController.merge(req, res));
 
 export default router;
