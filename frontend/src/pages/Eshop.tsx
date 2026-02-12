cf8a7f0f24a77e0aae53cb2a213448e1a1fe1d0c
yuliya.levandovskaya <yuliya.levandovskaya@gmail.com>
2026-02-12 09:59:06 +0100

    Revert "eshop"
    This reverts commit e553fb065b7408fda46d69c0c559bba49fc40bf8.

--- frontend/src/pages/Eshop.tsx
+++ frontend/src/pages/Eshop.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import ProductCatalog from '../components/eshop/productcatalog.tsx';
 
-const EShop: React.FC = () => {
+function Eshop() {
   return (
     <div className="p-6">
-      <h1 className="text-3xl font-bold mb-6">EShop</h1>
-      <p>This is the EShop page. Content coming soon...</p>
+      <h1 className="text-2xl font-bold mb-6">E-Shop</h1>
+      <ProductCatalog />
     </div>
   );
-};
+}
 
-export default EShop;
+export default Eshop;
