14c945e09a7ce799444bd45e50b25fda0797da78
antoinefoux-ui <antoine.foux@gmail.com>
2026-02-12 13:17:09 +0100

    Update client.routes.ts

--- backend/src/routes/client.routes.ts
+++ backend/src/routes/client.routes.ts
@@ -9,13 +9,13 @@ router.get('/', ClientController.getAll);
 
 // Protected routes
 router.post('/', authenticate, ClientController.create);
-router.get('/:id', authenticate, ClientController.getById);
-router.put('/:id', authenticate, ClientController.update);
-router.delete('/:id', authenticate, ClientController.delete);
 router.get('/:id/communications', authenticate, ClientController.getCommunications);
 router.post('/:id/communications', authenticate, ClientController.sendCommunication);
 router.get('/:id/statistics', authenticate, ClientController.getStatistics);
-router.post('/merge', authenticate, ClientController.merge);
 router.get('/:id/export', authenticate, ClientController.exportData);
+router.get('/:id', authenticate, ClientController.getById);
+router.put('/:id', authenticate, ClientController.update);
+router.delete('/:id', authenticate, ClientController.delete);
+router.post('/merge', authenticate, ClientController.merge);
 
 export default router;
